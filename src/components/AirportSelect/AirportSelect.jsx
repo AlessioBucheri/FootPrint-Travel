896f1934cd39da2c8f6cc0fc608c7e1154e86e4f
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-06-08 18:38:08 +0200

    edit jsx

--- src/components/AirportSelect/AirportSelect.jsx
+++ src/components/AirportSelect/AirportSelect.jsx
@@ -1,47 +1,27 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AsyncSelect from "react-select/async";
 import axios from "axios";
-import airportCodes from "airport-codes";
 
 const AirportSelect = ({ placeholder, onChange }) => {
-  const [inputValue, setInputValue] = useState("");
-
   const loadOptions = async (inputValue, callback) => {
-    // Cerca aeroporti in base alla città o al nome dell'aeroporto (o codice)
-    let allAirports = [];
-
-    // Se la lunghezza dell'input è almeno di 2 caratteri
-    if (inputValue.length >= 2) {
-      // Cerca per città
-      const airportsByCity = airportCodes.findWhere({ city: inputValue });
-      allAirports.push(...airportsByCity); // Aggiungi gli aeroporti della città al risultato
-
-      // Cerca con Travelpayouts (per nome aeroporto o codice IATA)
-      const response = await axios.get(
-        `https://autocomplete.travelpayouts.com/places2?term=${inputValue}&locale=en&types[]=airport`
-      );
-      const tpAirports = response.data
-        .filter((airport) => airport.code !== null)
-        .map((airport) => ({
-          ...airport,
-          city: airport.city_name, // Rinomina in 'city' per coerenza
-        }));
-      allAirports.push(...tpAirports);
+    if (inputValue.length < 2) {
+      callback([]);
+      return;
     }
 
-    // Rimuovi duplicati basandoti sul codice IATA
-    const uniqueAirports = allAirports.filter(
-      (airport, index, self) =>
-        index === self.findIndex((a) => a.iata === airport.iata)
-    );
-
-    // Formatta i risultati
-    const options = uniqueAirports.map((airport) => ({
-      value: airport.iata, // Il codice IATA viene passato come valore
-      label: `${airport.name} (${airport.iata}) - ${airport.city}`,
-    }));
-
-    callback(options);
+    try {
+      const response = await axios.get(
+        `https://autocomplete.travelpayouts.com/places2?term=${inputValue}&locale=en&type[]=airport`
+      );
+      const airports = response.data.map((airport) => ({
+        value: airport.code, // Assicurati che value sia il codice IATA
+        label: `${airport.name} (${airport.code})`,
+      }));
+      callback(airports);
+    } catch (error) {
+      console.error("Errore nel caricamento degli aeroporti", error);
+      callback([]);
+    }
   };
 
   return (
