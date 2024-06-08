a09b8320cbe7129ea40ae6a5fe63e3e5664ea0c5
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-06-08 18:28:07 +0200

    old version

--- src/components/AirportSelect/AirportSelect.jsx
+++ src/components/AirportSelect/AirportSelect.jsx
@@ -1,34 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import AsyncSelect from "react-select/async";
 import axios from "axios";
-import airportCodes from "airport-codes";
 
 const AirportSelect = ({ placeholder, onChange }) => {
-  const [inputValue, setInputValue] = useState("");
-
   const loadOptions = async (inputValue, callback) => {
-    // 1. Cerca aeroporti in base alla città o al nome dell'aeroporto
-    const airportsByCity = airportCodes.findWhere({ city: inputValue });
-    const airportsByName = airportCodes.findWhere({ name: inputValue });
-
-    // 2. Combina i risultati e rimuovi i duplicati
-    const allAirports = [...airportsByCity, ...airportsByName];
-    const uniqueAirports = allAirports.filter(
-      (airport, index, self) =>
-        index === self.findIndex((a) => a.iata === airport.iata)
-    );
-
-    // 3. Crea le opzioni per il menu a tendina
-    if (uniqueAirports.length > 0) {
-      const options = uniqueAirports.map((airport) => ({
-        value: airport.iata,
-        label: `${airport.name} (${airport.iata}) - ${airport.city}`,
-      }));
-      callback(options);
-      return;
-    }
-
-    // 4. Se non trovati, esegui la ricerca standard per aeroporto (come prima)
     if (inputValue.length < 2) {
       callback([]);
       return;
@@ -43,10 +18,6 @@ const AirportSelect = ({ placeholder, onChange }) => {
     }));
     callback(airports);
   };
-  // Aggiorna l'input value ogni volta che cambia
-  const handleInputChange = (newValue) => {
-    setInputValue(newValue);
-  };
 
   return (
     <AsyncSelect
@@ -54,15 +25,7 @@ const AirportSelect = ({ placeholder, onChange }) => {
       placeholder={placeholder}
       loadOptions={loadOptions}
       onChange={onChange}
-      onInputChange={handleInputChange} // Aggiungi il gestore per l'input
       noOptionsMessage={() => "Nessun aeroporto trovato"}
-      filterOption={(option, inputValue) => {
-        // Allow filtering by airport code or name
-        return (
-          option.value.toLowerCase().includes(inputValue.toLowerCase()) ||
-          option.label.toLowerCase().includes(inputValue.toLowerCase())
-        );
-      }}
     />
   );
 };
